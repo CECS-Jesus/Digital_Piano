2f24d3ae9156d852756fc759cf1b3c3fdc0ccf72
CECS-Jesus <119976553+CECS-Jesus@users.noreply.github.com>
2023-09-08 21:31:20 -0700

    Add files via upload

---
+++ Inc/switch.h
@@ -0,0 +1,13 @@
+#ifndef SWITCH_H
+#define SWITCH_H
+
+#define SW1 0x10
+#define SW2 0x01
+
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+void Switch_Init(void);
+
+#endif
